37d7b44e90c1b88b0b39e320091cdf451137dd45
Alvarogogal <agorsua0610@g.educaand.es>
2024-12-17 16:49:28 +0100

    .

--- .godot/mono/temp/obj/Debug/ProyectoPSP.AssemblyInfo.cs
+++ .godot/mono/temp/obj/Debug/ProyectoPSP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoPSP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d943c93102a0b4f7e231004c743a9b93e9a74804")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4530e65c0a882f5eaf671d585664b464d53c1ec3")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoPSP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoPSP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
